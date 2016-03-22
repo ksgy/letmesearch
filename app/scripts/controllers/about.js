c645f313ea66ce034f77c32ba310a001a1f059b3
ksgy <ksgy@x-plane.hu>
2016-03-22 19:56:01 +0000

    Initial project structure

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name letmesearchApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the letmesearchApp
+ */
+angular.module('letmesearchApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name letmesearchApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the letmesearchApp
+ */
+angular.module('letmesearchApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
